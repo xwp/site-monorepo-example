e8e6ae8c6b027ddcf899419af06e9bc4441d9b25
Kaspars Dambis <hi@kaspars.net>
2022-06-16 17:08:05 +0300

    Add more theme specific tests

--- wp-content/themes/sample-theme/tests/phpunit/class-theme-test.php
+++ wp-content/themes/sample-theme/tests/phpunit/class-theme-test.php
@@ -5,6 +5,8 @@
  * @package XWP\Sample_Theme
  */
 
+use XWP\Sample_Theme\Theme;
+
 /**
  * Test Theme.
  */
@@ -15,5 +17,33 @@ class Theme_Test extends WP_UnitTestCase {
      */
     public function test_wordpress_and_plugin_are_loaded() {
         $this->assertTrue( function_exists( 'do_action' ) );
+    }
+
+    /**
+     * Ensure the theme is loaded.
+     *
+     * @return void
+     */
+    public function test_theme_loaded() {
+        $this->assertTrue( class_exists( 'XWP\Sample_Theme\Theme' ) );
+    }
+
+    /**
+     * Can resolve asset paths and URLs relative to the theme root.
+     *
+     * @return void
+     */
+    public function test_asset_path_and_url_to() {
+        $theme = new Theme( '/path/to/theme' );
+
+        $this->assertEquals(
+            '/path/to/theme/asset/style.js',
+            $theme->path_to( 'asset/style.js' )
+        );
+
+        $this->assertEquals(
+            sprintf( '%s/asset/style.js', get_stylesheet_directory_uri() ),
+            $theme->url_to( 'asset/style.js' )
+        );
     }
 }
