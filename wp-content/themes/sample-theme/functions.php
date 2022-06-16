b581392f94b91d9d97c4d26dc01487dd6375bd09
Kaspars Dambis <hi@kaspars.net>
2022-06-16 19:30:14 +0300

    Add support for local autoload during tests

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -2,5 +2,10 @@
 
 namespace XWP\Sample_Theme;
 
+// Include the local autoloader only if the global one isn't available.
+if ( ! class_exists( 'XWP\Sample_Theme\Theme' ) && file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
 $theme = new Theme( __DIR__ );
 $theme->init();
