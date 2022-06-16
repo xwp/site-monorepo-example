cf73da0d719179b1dc677faab386794b3dc86444
Viktor Szépe <viktor@szepe.net>
2022-06-16 23:47:20 +0000

    Use ::class notation for referencing class names

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -3,7 +3,7 @@
 namespace XWP\Sample_Theme;
 
 // Include the local autoloader only if the global one isn't available.
-if ( ! class_exists( 'XWP\Sample_Theme\Theme' ) && file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+if ( ! class_exists( Theme::class ) && file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
