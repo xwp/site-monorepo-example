1d72fd644ed91d40a807b155f32702d2b20d13d2
Kaspars Dambis <hi@kaspars.net>
2022-06-16 22:14:21 +0300

    We need the autoloader for the tests, too

--- wp-tests-config.php
+++ wp-tests-config.php
@@ -6,14 +6,16 @@
  * phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
  */
 
+// Include the project-wide autoloader.
+require_once __DIR__ . '/vendor/autoload.php';
+
 define( 'ABSPATH', __DIR__ . '/wp/' );
 
 define( 'WP_DEBUG', true );
 
 define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ? getenv( 'WP_DB_NAME' ) : 'wp_phpunit_tests' );
 define( 'DB_USER', getenv( 'WP_DB_USER' ) ? getenv( 'WP_DB_USER' ) : 'root' );
-define( 'DB_PASSWORD', getenv( 'WP_DB_PASSWORD' ) ? getenv( 'WP_DB_PASSWORD' ) : '' );
-define( 'DB_HOST', getenv( 'WP_DB_HOST' ) ? getenv( 'WP_DB_HOST' ) : 'localhost' );
+define( 'DB_PASSWORD', getenv( 'WP_DB_PASSWORD' ) ? getenv( 'WP_DB_PASSWORD' ) : '' );define( 'DB_HOST', getenv( 'WP_DB_HOST' ) ? getenv( 'WP_DB_HOST' ) : 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
