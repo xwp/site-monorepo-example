9e70887698a44aff0a0f7f8451bff842ea3cdea5
Kaspars Dambis <hi@kaspars.net>
2022-08-13 13:14:21 +0300

    Support for parsing absolute paths

--- wp-content/themes/sample-theme/php/class-theme.php
+++ wp-content/themes/sample-theme/php/class-theme.php
@@ -11,6 +11,9 @@ class Theme {
     }
 
     public function url_to( $path_relative ) {
+        // Account for absolute paths to assets under the root directory.
+        $path_relative = str_replace( $this->dir, '', $path_relative );
+
         return sprintf(
             '%s/%s',
             get_template_directory_uri(),
@@ -19,6 +22,9 @@ class Theme {
     }
 
     public function path_to( $path_relative ) {
+        // Account for absolute paths to assets under the root directory.
+        $path_relative = str_replace( $this->dir, '', $path_relative );
+
         return sprintf(
             '%s/%s',
             $this->dir,
@@ -32,33 +38,6 @@ class Theme {
 
     public function asset_id( $id ) {
         return sprintf( '%s-%s', $this->basename(), $id );
-    }
-
-    public function init() {
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-    }
-
-    public function enqueue_scripts() {
-        $js_meta_file = $this->path_to( 'js/dist/index.asset.php' );
-
-        if ( is_readable( $js_meta_file ) ) {
-            $js_meta = file_get_contents( $js_meta_file );
-
-            wp_enqueue_script(
-                $this->asset_id( 'index' ),
-                $this->url_to( 'js/dist/index.js' ),
-                $js_meta['dependencies'],
-                $js_meta['version'],
-                true
-            );
-
-            wp_enqueue_style(
-                $this->asset_id( 'style-index' ),
-                $this->url_to( 'js/dist/style-index.css' ),
-                array(),
-                $js_meta['version']
-            );
-        }
     }
 
 }
