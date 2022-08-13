23cf32f06f8f30d71f791a812bbe25f462773523
Kaspars Dambis <hi@kaspars.net>
2022-08-13 11:18:21 +0300

    Set the CONTENT_URL too

--- wp-config.php
+++ wp-config.php
@@ -10,6 +10,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
 
+if ( ! empty( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ) {
+	define( 'WP_CONTENT_URL', sprintf( 'https://%s/wp-content', $_SERVER['SERVER_NAME'] ) );
+} else {
+	define( 'WP_CONTENT_URL', sprintf( 'http://%s/wp-content', $_SERVER['SERVER_NAME'] ) );
+}
+
 define( 'DB_NAME', getenv( 'WP_DB_NAME' ) );
 define( 'DB_USER', getenv( 'WP_DB_USER' ) );
 define( 'DB_PASSWORD', getenv( 'WP_DB_PASSWORD' ) );
