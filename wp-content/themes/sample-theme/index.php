969543a2e8d7fb3fbc80e38957c9b1994ccacdc5
Kaspars Dambis <hi@kaspars.net>
2022-08-13 13:16:17 +0300

    Some basic template to verify output

--- wp-content/themes/sample-theme/index.php
+++ wp-content/themes/sample-theme/index.php
@@ -4,6 +4,15 @@
         <?php wp_head(); ?>
     </head>
     <body>
+        <?php
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post();
+                the_title();
+                the_content();
+            }
+        }
+        ?>
         <?php wp_footer(); ?>
     </body>
 </html>
