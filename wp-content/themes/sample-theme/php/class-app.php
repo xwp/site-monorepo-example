dfa87591b990a2bb08559b4f0291d8d5df47c551
Kaspars Dambis <hi@kaspars.net>
2022-08-16 12:36:31 +0300

    Use helpers more

--- wp-content/themes/sample-theme/php/class-app.php
+++ wp-content/themes/sample-theme/php/class-app.php
@@ -28,16 +28,14 @@ class App {
     }
 
     public function enqueue_theme_assets() {
-        $js_meta_file = $this->theme->path_to( 'dist/js/index.asset.php' );
+        $index_js_meta = $this->asset_php( 'dist/js/index.js' );
 
-        if ( is_readable( $js_meta_file ) ) {
-            $js_meta = require $js_meta_file;
-
+        if ( ! empty( $index_js_meta['url'] ) ) {
             wp_enqueue_script(
                 $this->theme->asset_id( 'index' ),
-                $this->theme->url_to( 'dist/js/index.js' ),
-                $js_meta['dependencies'],
-                $js_meta['version'],
+                $index_js_meta['url'],
+                $index_js_meta['dependencies'],
+                $index_js_meta['version'],
                 true
             );
 
@@ -45,7 +43,7 @@ class App {
                 $this->theme->asset_id( 'style-index' ),
                 $this->theme->url_to( 'dist/js/style-index.css' ),
                 array(),
-                $js_meta['version']
+                $index_js_meta['version']
             );
         }
     }
@@ -88,18 +86,28 @@ class App {
         }
     }
 
-    protected function block_path_to_dist( $path ) {
-        return str_replace( $this->theme->dir(), $this->theme->path_to( 'dist' ), $path );
-    }
+    protected function asset_php( $path ) {
+        // Support paths relative to the theme root directory.
+        if ( 0 !== strpos( $path, $this->theme->dir() ) ) {
+            $path = $this->theme->path_to( $path );
+        }
 
-    protected function block_asset_php( $path, $deps = [] ) {
+        // Map to the PHP meta file name.
         $asset_php_path = str_replace( '.js', '.asset.php', $path );
-        $asset_php_path = $this->block_path_to_dist( $asset_php_path );
+
+        if ( ! is_readable( $asset_php_path ) ) {
+            return false;
+        }
 
         $meta = [
-            'dependencies' => $deps,
+            'dependencies' => [],
             'version' => filemtime( $path ),
+            'url' => $this->theme->url_to( $path ),
         ];
+
+        if ( in_array( 'wp-react-refresh-runtime', $meta['dependencies'] ) ) {
+            $meta['dependencies'][] = 'wp-react-refresh-entry';
+        }
 
         if ( is_readable( $asset_php_path ) ) {
             $asset_php_meta = require $asset_php_path;
@@ -111,16 +119,20 @@ class App {
         return $meta;
     }
 
+    protected function block_path_to_dist( $path ) {
+        return str_replace( $this->theme->dir(), $this->theme->path_to( 'dist' ), $path );
+    }
+
     public function enqueue_block_assets() {
         foreach ( $this->blocks as $block ) {
             $assets = $block->assets();
 
             if ( ! empty( $assets['script'] ) ) {
-                $asset_meta = $this->block_asset_php( $assets['script'] );
+                $asset_meta = $this->asset_php( $this->block_path_to_dist( $assets['script'] ) );
 
                 wp_enqueue_script(
                     $block->handle_for_context( 'script' ),
-                    $this->theme->url_to( $this->block_path_to_dist( $assets['script'] ) ),
+                    $asset_meta['url'],
                     $asset_meta['dependencies'],
                     $asset_meta['version'],
                     true
@@ -128,11 +140,11 @@ class App {
             }
 
             if ( ! empty( $assets['viewScript'] ) ) {
-                $asset_meta = $this->block_asset_php( $assets['viewScript'] );
+                $asset_meta = $this->asset_php( $this->block_path_to_dist( $assets['viewScript'] ) );
 
                 wp_enqueue_script(
                     $block->handle_for_context( 'viewScript' ),
-                    $this->theme->url_to( $this->block_path_to_dist( $assets['viewScript'] ) ),
+                    $asset_meta['url'],
                     $asset_meta['dependencies'],
                     $asset_meta['version'],
                     true
@@ -160,11 +172,11 @@ class App {
             $assets = $block->assets();
 
             if ( ! empty( $assets['editorScript'] ) ) {
-                $asset_meta = $this->block_asset_php( $assets['editorScript'] );
+                $asset_meta = $this->asset_php( $assets['editorScript'] );
 
                 wp_enqueue_script(
                     $block->handle_for_context( 'editorScript' ),
-                    $this->theme->url_to( $this->block_path_to_dist( $assets['editorScript'] ) ),
+                    $asset_meta['url'],
                     $asset_meta['dependencies'],
                     $asset_meta['version'],
                     true
