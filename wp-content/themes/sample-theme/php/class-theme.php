717e85d2a95ffdb7d23c3428a6bae35c73813f99
Kaspars Dambis <hi@kaspars.net>
2022-08-16 09:27:01 +0300

    Cache the URI reference

--- wp-content/themes/sample-theme/php/class-theme.php
+++ wp-content/themes/sample-theme/php/class-theme.php
@@ -6,24 +6,39 @@ class Theme {
 
     protected $dir;
 
+    protected $dir_uri;
+
     public function __construct( $dir ) {
         $this->dir = rtrim( $dir, '\\/' );
+        $this->dir_uri = rtrim( get_template_directory_uri(), '\\/' );
     }
 
-    public function url_to( $path_relative ) {
+    public function dir() {
+        return $this->dir;
+    }
+
+    public function url_to( $path_relative = null ) {
         // Account for absolute paths to assets under the root directory.
         $path_relative = str_replace( $this->dir, '', $path_relative );
 
+        if ( empty( $path_relative ) ) {
+            return $this->dir_uri;
+        }
+
         return sprintf(
             '%s/%s',
-            get_template_directory_uri(),
+            $this->dir_uri,
             ltrim( $path_relative, '\\/' )
         );
     }
 
-    public function path_to( $path_relative ) {
+    public function path_to( $path_relative = null ) {
         // Account for absolute paths to assets under the root directory.
         $path_relative = str_replace( $this->dir, '', $path_relative );
+
+        if ( empty( $path_relative ) ) {
+            return $this->dir;
+        }
 
         return sprintf(
             '%s/%s',
